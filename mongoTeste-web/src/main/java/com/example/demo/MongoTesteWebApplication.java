694927a0c4ab832f784e861aac01d58a485a4e1f
matrix10 <igor_lealnodari@hotmail.com>
2019-08-09 15:40:06 -0300

    Spring with mongoDB

---
+++ mongoTeste-web/src/main/java/com/example/demo/MongoTesteWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoTesteWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoTesteWebApplication.class, args);
+	}
+
+}
